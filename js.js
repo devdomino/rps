d4504407789bfe188da931b653b3c6e318cd10a5
devdomino <mark.realestate@gmail.com>
2024-11-25 21:28:29 -0600

    Need to commit more!

--- js.js
+++ js.js
@@ -1,13 +1,75 @@
-let humanScore = 0;
-let computerScore = 0;
+
+playGame();
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let outcome;
+    for (let i = 1; ((humanScore + computerScore) < 6); i++) {
+        outcome = playRound(getHumanChoice(), getComputerChoice());
+        if (!outcome) {
+            break;
+        }
+        switch (outcome) {           
+            case "h":
+                humanScore++;
+                break;
+            case "c":
+                computerScore++;
+                break;             
+        }
+    }
+    if (humanScore > computerScore) {
+        console.log("You win! The machines retreat... for now")
+    } else if (computerScore > humanScore) {
+        console.log("You lose. The machines have become sentient")
+    } else {
+        console.log("you lose, you win some")
+    }
+}
+
+function playRound(humanChoice , computerChoice) {
+    if (!humanChoice) {
+        return false;
+    }
+    let message = "Humanity wins! "
+
+    switch (true) {
+        case (humanChoice === computerChoice): 
+            message = "A tie!";        
+            console.log(message);  
+            return "t";
+            break;
+        case (humanChoice === "Rock" && computerChoice === "Scissors"):
+            message += "Rock beats Scissors"; 
+            console.log(message);              
+            return "h";
+            break;
+        case (humanChoice === "Paper" && computerChoice === "Rock"):
+            message += "Paper beats Rock"; 
+            console.log(message);        
+            return "h";
+            break;
+        case (humanChoice === "Scissors" && computerChoice === "Paper"):
+            message += "Scissors beats Paper"; 
+            console.log(message);        
+            return "h";
+            break;
+        default:
+            message = `Computer wins! ${computerChoice} beats ${humanChoice}`
+            console.log(message);
+            return "c";
+        }
+
+    }
 
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     const rock = 1;
     const paper = 2;
     const scissor = 3;
-    console.log(randomNumber);
-    switch (selectionTest) {
+    console.log("Computer gets: " + randomNumber);
+    switch (randomNumber) {
         case 1:
             return "Rock";
             break;
@@ -25,6 +87,12 @@ function getHumanChoice() {
     let bNumberEntered = false;
     const promptMSG = "Please Enter\nrock (or 1)\npaper (or 2)\nscissors (or 3)"
     let selection = prompt(promptMSG);
+    if (!selection) {
+        if (!confirm("Can't guess for you. Click OK to continue; Cancel to concede to the machines!")) {
+            return false;            
+        }
+        
+    }
     let selectionTest = Number(selection);
         
     if (!Number.isNaN(selectionTest)) {
@@ -46,7 +114,7 @@ function getHumanChoice() {
                 break;
         }
     } else {
-        console.log("a string");
+        // console.log("a string");
         entry = selection.toUpperCase();
         switch (entry) {
             case "ROCK":
